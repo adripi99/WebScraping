e0f498b030fbb207f93af34ea8d259b4dc0e7f03
Adrian Picos <15playstore@gmail.com>
2023-07-16 19:15:40 +0200

    comentado aliweb

--- Objects/Web/aliexpress_web.py
+++ Objects/Web/aliexpress_web.py
@@ -11,6 +11,17 @@ class AliexpressWeb(Web):
         super().__init__(show_browser)
     
     def extraer_atributos_producto(self, elemento, atributosP,i):
+        """
+        Extrae los atributos del producto de un elemento dado.
+
+        Args:
+            elemento (WebElement): Elemento del producto.
+            atributosP (bool): Indica si se deben extraer atributos en profundidad.
+            i (int): Índice del elemento en la lista de elementos.
+
+        Returns:
+            dict: Diccionario con los atributos extraídos del producto.
+        """
         atributos_extraidos = {}
         try:
             titulo = elemento.find_element(By.XPATH, './/h1[@class="manhattan--titleText--WccSjUS"]').text
@@ -27,9 +38,13 @@ class AliexpressWeb(Web):
             atributos_extraidos["Precio"] = "No encontrado"
 
         if atributosP:
-            url_elemento = self.driver.find_elements(By.XPATH, '//a[contains(@class,"earch-card-item")]')[i]
-            url = url_elemento.get_attribute('href')
-            self.driver.get(url)
+            try:
+                url_elemento = self.driver.find_elements(By.XPATH, '//a[contains(@class,"earch-card-item")]')[i]
+                url = url_elemento.get_attribute('href')
+                self.driver.get(url)
+            except Exception as e:
+                print("No se pudo entrar en profundidad")
+            sleep(2)#esperamos a que cargue la página
             try:
                 atributos_extraidos["Estrellas"] = self.driver.find_element(By.XPATH, './/div[@class="ae-header-content-num"]').text
 
@@ -40,35 +55,63 @@ class AliexpressWeb(Web):
                 atributos_extraidos["Vendedor"] = self.driver.find_element(By.XPATH, './/a[@class="store-header--storeName--vINzvPw"]').text
             except Exception as e:
                 atributos_extraidos["Vendedor"] = "No encontrado"
-
-            self.driver.back()
+            try:
+                self.driver.back()
+            except Exception as e:
+                print("No se pudo volver para atrás")
         return atributos_extraidos
     
     def buscar_productos(self, categoria, num_productos,atributos_en_profundidad,atributos_a_extraer, log_callback=None):
-        self.configurar_navegador()
-        url = self.obtener_url_aliexpress(categoria)
-        self.driver.get("https://es.aliexpress.com")
-        sleep(1)
-        accept_button = self.driver.find_element(By.XPATH, './/button[@class="btn-accept"]')
-        accept_button.click()
-        sleep(1)
-        self.driver.get(url)
-        self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
-        self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
+        """
+        Realiza la búsqueda de productos en la página de Aliexpress.
+
+        Args:
+            categoria (str): Categoría de productos a buscar.
+            num_productos (int): Número de productos a buscar.
+            atributos_en_profundidad (bool): Indica si se deben extraer atributos en profundidad.
+            atributos_a_extraer (list): Lista de atributos a extraer de los productos.
+            log_callback (func): Función de devolución de llamada para el registro de eventos.
+
+        Returns:
+            ColeccionProductos: Colección de productos encontrados.
+        """
+        try:
+            self.configurar_navegador()
+        except Exception as e:
+            print("Fallo al configurar el navegador")
+        try:
+            url = self.obtener_url_aliexpress(categoria)
+            self.driver.get("https://es.aliexpress.com")
+            sleep(1)
+            # Aceptar las cookies
+            try:
+                accept_button = self.driver.find_element(By.XPATH, './/button[@class="btn-accept"]')
+                accept_button.click()
+            except Exception as e:
+                print("No se encontró el botón de aceptar")
+            sleep(1)
+            self.driver.get(url)
+            self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)#Subimos y bajamos para que cargen todos los elementos de la pagina
+            self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
+        except Exception as e:
+            print("Fallo al obtener la url")
         productos = ColeccionProductos(atributos_a_extraer)
         Numero_Productos = 0
 
         while Numero_Productos != num_productos:
-            self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
-            self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
-            wait = WebDriverWait(self.driver, 20)
-            elementosList = wait.until(EC.presence_of_all_elements_located((By.XPATH,'//a[contains(@class,"earch-card-item")]')))
-
+            try:
+                self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
+                self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
+                wait = WebDriverWait(self.driver, 20)
+                elementosList = wait.until(EC.presence_of_all_elements_located((By.XPATH,'//a[contains(@class,"earch-card-item")]')))
+            except Exception as e:
+                print("Fallo al obtener los elementos")
+                self.cerrar_navegador()
+                return productos
             for i in range(len(elementosList)):
-                print(i)
                 try:
                     elemento=self.driver.find_elements(By.XPATH, '//a[contains(@class,"earch-card-item")]')[i]
-                except:
+                except:#Esto es necesario porque hay veces que dejan de aparecer todos los elemenos cuando estamos de la mitad para abajo
                     self.driver.refresh()
                     sleep(1)
                     self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)
@@ -94,11 +137,13 @@ class AliexpressWeb(Web):
                                 return productos
                         print("Error al encontrar el elemento:"+str(i))
                         return productos
-                atributos_extraidos = self.extraer_atributos_producto(elemento,atributos_en_profundidad,i)
-                producto = AliexpressProducto(**atributos_extraidos)
-                #print(atributos_extraidos)
-                productos.agregar_producto(producto)
-                Numero_Productos += 1
+                try:
+                    atributos_extraidos = self.extraer_atributos_producto(elemento,atributos_en_profundidad,i)
+                    producto = AliexpressProducto(**atributos_extraidos)
+                    productos.agregar_producto(producto)
+                    Numero_Productos += 1
+                except Exception as e:
+                    print("Fallo al guardar el elemento")
                 if log_callback is not None:
                     try:
                         sleep(1)
@@ -139,6 +184,15 @@ class AliexpressWeb(Web):
         return productos
 
     def obtener_url_aliexpress(self, categoria):
+        """
+        Obtiene la URL correspondiente a una categoría de productos en Aliexpress.
+
+        Args:
+            categoria (str): Categoría de productos.
+
+        Returns:
+            str: URL de la categoría en Aliexpress.
+        """
         categorias_urls = {
             "cocina": "https://es.aliexpress.com/category/204000021/consumer-electronics.html",
             "ordenadores": "https://es.aliexpress.com/category/204000007/computer-office.html?category_redirect=1"
